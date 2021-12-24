024630e968c2d2334236c92c3898f7b175483c64
Brian Kiptoo <77725948+brian-002@users.noreply.github.com>
2021-12-24 18:24:44 +0300

    Add files via upload

---
+++ questionnaire.js
@@ -0,0 +1,23 @@
+let message= document.getElementById("msg")
+function submmit() {
+    message.textContent="submmision succesful\nthankyou for your time"
+}
+
+let ans10=document.getElementById("quiz-10")
+function tenans(){
+ans10.textContent=prompt("your suggestions")}
+
+let ans8=document.getElementById("explainon8")
+function answer(){
+    ans8.textContent=prompt("explain")
+}
+
+let ans1=document.getElementById("explainon1")
+function answer1(){
+    ans1.textContent=prompt("explain")
+}
+
+let ans9=document.getElementById("explainon9")
+function answer9() {
+    ans9.textContent=prompt("explain")
+}
